734641fe90a4427b91fdac00155be87b3314472b
HobisJung <hobisjung@gmail.com>
2015-04-06 09:59:00 +0900

    2015-04-06

---
+++ TryBase64/TryBase64/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(328, 55)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Padding = New System.Windows.Forms.Padding(0, 2, 0, 0)
+        Me.Button1.Size = New System.Drawing.Size(60, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.TabStop = False
+        Me.Button1.Text = "Clear"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(81, 12)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "● Target File"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 55)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Padding = New System.Windows.Forms.Padding(0, 2, 0, 0)
+        Me.Button3.Size = New System.Drawing.Size(200, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.TabStop = False
+        Me.Button3.Text = "▼▼▼    Save Text File"
+        Me.Button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(376, 21)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(262, 55)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Padding = New System.Windows.Forms.Padding(0, 2, 0, 0)
+        Me.Button2.Size = New System.Drawing.Size(60, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.TabStop = False
+        Me.Button2.Text = "Open"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 84)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Padding = New System.Windows.Forms.Padding(0, 2, 0, 0)
+        Me.Button4.Size = New System.Drawing.Size(200, 23)
+        Me.Button4.TabIndex = 10
+        Me.Button4.TabStop = False
+        Me.Button4.Text = "▼▼▼    Save Binary File"
+        Me.Button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(400, 120)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ TryBase64/TryBase64/Form1.vb
@@ -0,0 +1,174 @@
+﻿Imports System.Threading
+
+Public NotInheritable Class Form1
+
+    ' ::
+    Private Sub p_Me_Load( _
+                    ByVal sender As Object, _
+                    ByVal e As EventArgs) Handles MyBase.Load
+        '
+        Me.Text = "TryBase64 ver 1.04"
+        Me.StartPosition = FormStartPosition.Manual
+        Me.Location = New Point(100, 100)
+        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        '
+        Me.Label2.Text &= " (I wanna be drag and drop files.)"
+        '
+        Me.TextBox1.BorderStyle = BorderStyle.FixedSingle
+        Me.TextBox1.WordWrap = False
+        Me.TextBox1.ScrollBars = ScrollBars.Both
+        Me.TextBox1.TabIndex = 1
+
+        '
+        Me.AllowDrop = True
+        AddHandler Me.DragEnter, AddressOf Me.p_Me_DragEnter
+        AddHandler Me.DragDrop, AddressOf Me.p_Me_DragDrop
+    End Sub
+
+    ' ::
+    Private Sub p_Me_DragEnter(ByVal sender As Object, ByVal dea As DragEventArgs)
+        If (dea.Data.GetDataPresent(DataFormats.FileDrop)) Then
+            dea.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    ' ::
+    Private Sub p_Me_DragDrop(ByVal sender As Object, ByVal dea As DragEventArgs)
+        Dim t_dragObjs As Object = dea.Data.GetData(DataFormats.FileDrop)
+        Dim t_filePaths() As String = TryCast(t_dragObjs, String())
+        If (t_filePaths.Length = 1) Then
+            Dim t_filePath As String = t_filePaths(0)
+            Me.OpenFileDialog1.FileName = t_filePath
+            Me._TargetFilePath = t_filePath
+            Me.TextBox1.Text = Me._TargetFilePath
+        End If
+    End Sub
+
+    ' :: Clear
+    Private Sub p_Button1_Click( _
+                    ByVal sender As Object, _
+                    ByVal e As EventArgs) Handles Button1.Click
+        '
+        Me.TextBox1.Clear()
+        Me._TargetFilePath = Nothing
+    End Sub
+
+    Private _TargetFilePath As String = Nothing
+    ' :: Open
+    Private Sub p_Button2_Click( _
+                    ByVal sender As Object, _
+                    ByVal e As EventArgs) Handles Button2.Click
+        '
+        Dim t_dr As DialogResult = Me.OpenFileDialog1.ShowDialog(Me)
+        If (t_dr.Equals(DialogResult.OK)) Then
+            Me._TargetFilePath = Me.OpenFileDialog1.FileName
+            Me.TextBox1.Text = Me._TargetFilePath
+        End If
+    End Sub
+
+    ' ::
+    Private Sub p_Button3_Click( _
+                    ByVal sender As Object, _
+                    ByVal e As EventArgs) Handles Button3.Click
+        '
+        MBase64Task.fToString(Me._TargetFilePath)
+    End Sub
+
+    ' ::
+    Private Sub p_Button4_Click( _
+                    ByVal sender As Object, _
+                    ByVal e As EventArgs) Handles Button4.Click
+        '
+        MBase64Task.fToBinary(Me._TargetFilePath)
+    End Sub
+
+End Class
+
+
+Module MBase64Task
+
+    ' -
+    Private _WorkThread As Thread = Nothing
+    ' -
+    Private _TargetFilePath As String = Nothing
+
+    ' -
+    Private Const _EndString_Base64String As String = "_Base64String"
+    ' -
+    Private Const _EndString_Base64Binary As String = "_Base64Binary"
+
+
+    ' ::
+    Public Sub fToString(ByVal TargetFilePath As String)
+        If (TargetFilePath Is Nothing) Then Exit Sub
+
+        If (Not My.Computer.FileSystem.FileExists(TargetFilePath)) Then Exit Sub
+
+        If (_WorkThread Is Nothing) Then
+            _TargetFilePath = TargetFilePath
+            _WorkThread = New Thread(AddressOf p_ToString)
+            _WorkThread.IsBackground = True
+            _WorkThread.Start()
+        End If
+    End Sub
+    ' ::
+    Private Sub p_ToString()
+        Dim t_SaveFilePath As String = Nothing
+
+        If (My.Computer.FileSystem.FileExists(_TargetFilePath)) Then
+            Try
+                t_SaveFilePath = _TargetFilePath & _EndString_Base64String
+                Dim t_Bytes() As Byte = My.Computer.FileSystem.ReadAllBytes(_TargetFilePath)
+                Dim t_SaveStr As String = Convert.ToBase64String(t_Bytes, 0, t_Bytes.Length, Base64FormattingOptions.InsertLineBreaks)
+                My.Computer.FileSystem.WriteAllText(t_SaveFilePath, t_SaveStr, False, System.Text.Encoding.UTF8)
+            Catch
+                t_SaveFilePath = Nothing
+            End Try
+        End If
+        _WorkThread = Nothing
+        _TargetFilePath = Nothing
+
+        If (Not t_SaveFilePath Is Nothing) Then
+            MessageBox.Show("Completed As File: " & vbCrLf & t_SaveFilePath, "Notifications")
+        End If
+    End Sub
+
+
+    ' ::
+    Public Sub fToBinary(ByVal TargetFilePath As String)
+        If (TargetFilePath Is Nothing) Then Exit Sub
+
+        If (Not My.Computer.FileSystem.FileExists(TargetFilePath)) Then Exit Sub
+
+        If (_WorkThread Is Nothing) Then
+            _TargetFilePath = TargetFilePath
+            _WorkThread = New Thread(AddressOf p_ToBinary)
+            _WorkThread.IsBackground = True
+            _WorkThread.Start()
+        End If
+    End Sub
+    ' ::
+    Private Sub p_ToBinary()
+        Dim t_SaveFilePath As String = Nothing
+
+        If (My.Computer.FileSystem.FileExists(_TargetFilePath)) Then
+            Try
+                t_SaveFilePath = _TargetFilePath & _EndString_Base64Binary
+                Dim t_Str As String = My.Computer.FileSystem.ReadAllText(_TargetFilePath)
+                Dim t_SaveBytes() As Byte = Convert.FromBase64String(t_Str)
+                My.Computer.FileSystem.WriteAllBytes(t_SaveFilePath, t_SaveBytes, False)
+            Catch
+                t_SaveFilePath = Nothing
+            End Try
+        End If
+        _WorkThread = Nothing
+        _TargetFilePath = Nothing
+
+        If (Not t_SaveFilePath Is Nothing) Then
+            MessageBox.Show("Completed As File: " & vbCrLf & t_SaveFilePath, "Notifications")
+        End If
+    End Sub
+
+End Module
+
